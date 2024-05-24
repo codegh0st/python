f1e901bf81bccb90a832f5b7a2840619645d7cab
foxinious <foxysync@gmail.com>
2024-05-24 09:35:01 +0530

    added regex examples, added some regex docs

--- regular-expresion.py
+++ regular-expresion.py
@@ -3,6 +3,26 @@
 #REGULAR EXPRESION
 #----------------------
 '''
+import re
+
+re.compile() - creates object pattern using given paterns
+re.match() - matches only beggining of the string. r'p', 'python' -> start searching from first char of python, return match obj
+re.search() - scan the whole string but returns 'match object' of only 1st occurence
+re.findall() - scan all string, and retruns matching values as list
+re.fullmatch() - patter should match exactly in search string. no other char shuld be there in string other than patern, it acts liek comparision operator e.g <seach-patern> == <search-string> if patrn matches, will return 'match object', if not reruns None
+re.split() -
+re.subn() -
+
+Note: Repetaion charectors and its meaning
+* -> 0 to n number of char repetation
++ -> 1 to n number of char repetation
+? -> 0 to 1 number of repitation
+{m} -> exactly m number of repetition
+{m, n} -> from m minimum to n maximum number fo times of repetation
+{m,n}? -> m minimum number of repetition
+{m,n}+ -> n maximum number of repetaion
+
+
 What is regular expression?
 Regular expression is a special string which define search pattern or match pattern or split pattern.
 Regular expression (regex), defines a pattern which is used to search or match with string.
@@ -30,8 +50,63 @@ OBSERVE THE EXAMPLES TO UNDERSTAND
 '''
 
 
+
 #----------------------
 
+
+
+#----------------------
+import re
+
+names = ["rahul", "ramu", "ramesh", "suresh", "indran"]
+
+for name in names:
+    m = re.findall(r'^[rs].*h$', name) # name starting with r or s, followed by any char any number of times in between, but should end with h.
+    if m!= []: #if m is not empty means if there is value in 'm' then print 'm'
+        print(m)
+
+#----------------------
+# #{m,n}? - returns minimum number of occurence
+# import re
+
+# list1 = re.findall(r'a{2,5}?', "aaab a aaaaab aabaaaaaab")
+# list2 = re.findall(r'a{2,5}+', "aaab a aaaaab aabaaaaaab")
+# print(list1)
+# print(list2)
+
+# # OUTPUT:
+# # ['aa', 'aa', 'aa', 'aa', 'aa', 'aa', 'aa']
+# # ['aaa', 'aaaaa', 'aa', 'aaaaa']
+
+
+#----------------------
+# #{m, n} -> exact range of charetor
+# import re
+# str1 = "aab ab aaab aaaaab aaabaabaa aaaabaaabaaaaab"
+
+# list1 = re.search(r'a{4,}', str1)
+# print(list1)
+
+
+#----------------------
+# #re.fullmatch()
+# import re
+
+# m = re.fullmatch(r'py.*', "python")
+# print(m.group(0))
+
+#----------------------
+# import re
+# courses = ["python", "java", "cnn", "jython"]
+# for course in courses:
+#     m = re.fullmatch(r'.{3,}n$', course) #any char with 3 char minimum, should ends with n
+#     if m!=None:
+#         print(m.group(0))
+
+# #output:
+# #python
+# #c+nn
+# #jython
 
 
 #----------------------
